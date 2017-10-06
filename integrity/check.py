0a243bb320825882b016edc5557021212711f0ad
Keith Goodman <kwgoodman@gmail.com>
2017-10-06 09:34:00 -0700

    add more integrity tests

--- integrity/check.py
+++ integrity/check.py
@@ -21,8 +21,17 @@ def eras(data):
 def features(data):
 
     # feature bounds
-    for i, f in data.feature_iter():
-        array_interval('feature %2d' % i, 'critical', f, [0, 1])
+    for feature_num, x in data.feature_iter():
+        array_interval('feature %2d' % feature_num, x, [0, 1])
+
+    # distribution of each feature in each era
+    for era, feature_num, x in data.era_feature_iter():
+
+        msg = 'mean of feature %2d in %s' % (feature_num, era.ljust(6))
+        interval(msg, x.mean(), [0.4545, 0.5505])
+
+        msg = 'std  of feature %2d in %s' % (feature_num, era.ljust(6))
+        interval(msg, x.std(), [0.09, 0.14])
 
 
 def regions(data):
@@ -40,7 +49,14 @@ def predictions(data):
 # ---------------------------------------------------------------------------
 # logging utilities
 
-def array_interval(message, level, arr, limit):
+def interval(message, value, limit, level='warn'):
+    if value < limit[0] or value > limit[1]:
+        log = get_logger(level)
+        fmt = message + " %7.4f not in %s"
+        log(fmt % (value, str(limit)))
+
+
+def array_interval(message, arr, limit, level='warn'):
     amin, amax = arr.min(), arr.max()
     if amin < limit[0] or amax > limit[1]:
         log = get_logger(level)
